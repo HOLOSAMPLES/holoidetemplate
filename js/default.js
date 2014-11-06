83bc1621e2d2b0c8e24ce8dfbbcab98e7b994d44
HOLOSAMPLES <hanlong@notmatter.com>
2014-11-06 13:18:02 +0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -23,24 +23,33 @@ function Init(){
  		document.body.appendChild( renderer.domElement );
   
        //add object to Scene
-      var graph = new THREE.Mesh(new THREE.SphereGeometry(8, 30, 10), new THREE.MeshLambertMaterial({color:0xffffff}));
-	  scene.add(graph);
+        addObjectsToScene();
   
         //add Light
- 		var xl = new THREE.DirectionalLight( 0x555555 );
- 		xl.position.set( 1, 0, 2 );
- 		scene.add( xl );
- 		var pl = new THREE.PointLight(0x111111);
- 		pl.position.set(-20, 10, 20);
- 		scene.add(pl);
- 		var ambientLight = new THREE.AmbientLight(0x111111);	
- 		scene.add(ambientLight);
+ 		addLights();
  }
 
  function animate() 
  {
  	requestAnimationFrame( animate );
-  
     renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
 	renderer.Leia_render(scene, camera,undefined,undefined,_holoScreenScale,_camFov,_messageFlag);
  }
+
+function addObjectsToScene(){
+    //Add your objects here
+    var graph = new THREE.Mesh(new THREE.SphereGeometry(8, 30, 10), new   THREE.MeshLambertMaterial({color:0xffffff}));
+	  scene.add(graph);
+}
+
+function addLights(){
+    //Add Lights Here
+    var xl = new THREE.DirectionalLight( 0x555555 );
+ 	xl.position.set( 1, 0, 2 );
+ 	scene.add( xl );
+ 	var pl = new THREE.PointLight(0x111111);
+ 	pl.position.set(-20, 10, 20);
+ 	scene.add(pl);
+ 	var ambientLight = new THREE.AmbientLight(0x111111);	
+ 	scene.add(ambientLight);
+}
